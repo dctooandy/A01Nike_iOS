b3401ab180707d6a640c5a828f65b796b9c7ec98
cean.q <>
2018-11-27 09:48:50 +0800

    扫码选择渠道页面

---
+++ Hybird_A01/Payment/View/CNPayQRCell.h
@@ -0,0 +1,17 @@
+//
+//  CNPayQRCell.h
+//  Hybird_A01
+//
+//  Created by cean.q on 2018/11/26.
+//  Copyright © 2018 BTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CNPayQRCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hybird_A01/Payment/View/CNPayQRCell.m
@@ -0,0 +1,18 @@
+//
+//  CNPayQRCell.m
+//  Hybird_A01
+//
+//  Created by cean.q on 2018/11/26.
+//  Copyright © 2018 BTT. All rights reserved.
+//
+
+#import "CNPayQRCell.h"
+
+@implementation CNPayQRCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
