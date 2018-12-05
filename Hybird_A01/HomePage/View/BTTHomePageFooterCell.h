4f5f400bb23990d001fa8e8c9527b2a508cd6084
Domino <domino.l@iv66.net>
2018-12-05 14:51:15 +0800

    修复bugs

---
+++ Hybird_A01/HomePage/View/BTTHomePageFooterCell.h
@@ -0,0 +1,17 @@
+//
+//  BTTHomePageFooterCell.h
+//  Hybird_A01
+//
+//  Created by Domino on 05/12/2018.
+//  Copyright © 2018 BTT. All rights reserved.
+//
+
+#import "BTTBaseCollectionViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BTTHomePageFooterCell : BTTBaseCollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
