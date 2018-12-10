a1a90b879857e5726940a88cc16070ed184307ee
Domino <domino.l@iv66.net>
2018-12-10 10:32:30 +0800

    添加博币兑换提示框

---
+++ Hybird_A01/LuckyWheel/View/BTTLuckyWheelCoinView.h
@@ -0,0 +1,17 @@
+//
+//  BTTLuckyWheelCoinView.h
+//  Hybird_A01
+//
+//  Created by Domino on 10/12/2018.
+//  Copyright © 2018 BTT. All rights reserved.
+//
+
+#import "BTTBaseAnimationPopView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BTTLuckyWheelCoinView : BTTBaseAnimationPopView
+
+@end
+
+NS_ASSUME_NONNULL_END
