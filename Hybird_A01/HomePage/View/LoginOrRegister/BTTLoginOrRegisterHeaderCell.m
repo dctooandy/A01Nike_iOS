e5333d3bc6326d243ab6751c27592a8bfedbae09
Domino <domino.l@iv66.net>
2019-03-27 11:44:00 +0800

    版本号展示

--- Hybird_A01/HomePage/View/LoginOrRegister/BTTLoginOrRegisterHeaderCell.m
+++ Hybird_A01/HomePage/View/LoginOrRegister/BTTLoginOrRegisterHeaderCell.m
@@ -8,11 +8,27 @@
 
 #import "BTTLoginOrRegisterHeaderCell.h"
 
+@interface BTTLoginOrRegisterHeaderCell ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *iconImage;
+
+
+@end
+
 @implementation BTTLoginOrRegisterHeaderCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.mineSparaterType = BTTMineSparaterTypeNone;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap)];
+    self.iconImage.userInteractionEnabled = YES;
+    tap.numberOfTapsRequired = 6;
+    [self.iconImage addGestureRecognizer:tap];
+}
+
+- (void)tap {
+    [MBProgressHUD showMessagNoActivity:[NSString stringWithFormat:@"当前版本是: %@",app_version] toView:nil];
 }
 
 @end
