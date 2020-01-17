f1ecaeaf3d3891041df0832d961d880ea2526b71
levy <levy@iv66.net>
2020-01-17 18:18:24 +0800

    春节装饰

--- Hybird_A01/HomePage/View/BTTHomePageHeaderView.m
+++ Hybird_A01/HomePage/View/BTTHomePageHeaderView.m
@@ -41,15 +41,15 @@
         case BTTNavTypeHomePage:
         {
             // 中秋装饰
-//            NSString *path = nil;
-//            if (KIsiPhoneX) {
-//                path = [[NSBundle mainBundle] pathForResource:@"828x176-min" ofType:@"gif"];;
-//            } else {
-//                path = [[NSBundle mainBundle] pathForResource:@"828x128-min" ofType:@"gif"];;
-//            }
-//            NSData *data = [NSData dataWithContentsOfFile:path];
-//            UIImage *image = [UIImage sd_animatedGIFWithData:data];
-//            [self sd_setImageWithURL:nil placeholderImage:image];
+            NSString *path = nil;
+            if (KIsiPhoneX) {
+                path = [[NSBundle mainBundle] pathForResource:@"828x176-min" ofType:@"gif"];;
+            } else {
+                path = [[NSBundle mainBundle] pathForResource:@"828x128-min" ofType:@"gif"];;
+            }
+            NSData *data = [NSData dataWithContentsOfFile:path];
+            UIImage *image = [UIImage sd_animatedGIFWithData:data];
+            [self sd_setImageWithURL:nil placeholderImage:image];
 //            UIImageView *moonImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"moon"]];
 //            if (KIsiPhoneX) {
 //                moonImage.frame = CGRectMake(55, 15, 50, 50);
@@ -59,13 +59,13 @@
 //            [self addSubview:moonImage];
             
             
-            UIImageView *logoImageView = [[UIImageView alloc] initWithFrame:CGRectMake(BTTLeftConstants, BTTIconTop + (64 - 30) / 2 + 5, 80, 30)];
+            UIImageView *logoImageView = [[UIImageView alloc] initWithFrame:CGRectMake((SCREEN_WIDTH - 90) / 2, BTTIconTop + (64 - 30) / 2 + 5, 90, 30)];
             [self addSubview:logoImageView];
             logoImageView.image = ImageNamed(@"Navlogo");
             
             
             self.titleLabel = [UILabel new];
-            [self addSubview:self.titleLabel];
+//            [self addSubview:self.titleLabel];
             self.titleLabel.frame = CGRectMake((SCREEN_WIDTH - 150) / 2, BTTIconTop + (64 - 18) / 2 + 10, 150, 18);
             self.titleLabel.text = @"首页";
             self.titleLabel.textAlignment = NSTextAlignmentCenter;
