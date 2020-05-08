92e28b4efa1f2ec67a9a7c17890f736c579dc52c
levy <levy@iv66.net>
2020-05-08 10:30:24 +0800

    补充修复

--- Hybird_A01/Payment/VC/USDTPay/CNPayUSDTQRSecondVC.m
+++ Hybird_A01/Payment/VC/USDTPay/CNPayUSDTQRSecondVC.m
@@ -108,7 +108,7 @@
     //Gradient 0 fill for 圆角矩形 11
     CAGradientLayer *gradientLayer0 = [[CAGradientLayer alloc] init];
     gradientLayer0.cornerRadius = 5;
-    gradientLayer0.frame = _confirmBtn.bounds;
+    gradientLayer0.frame = CGRectMake(0, 0, SCREEN_WIDTH-30, 44);
     gradientLayer0.colors = @[
         (id)[UIColor colorWithRed:247.0f/255.0f green:249.0f/255.0f blue:82.0f/255.0f alpha:1.0f].CGColor,
         (id)[UIColor colorWithRed:242.0f/255.0f green:218.0f/255.0f blue:15.0f/255.0f alpha:1.0f].CGColor];
