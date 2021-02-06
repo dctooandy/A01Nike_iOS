93ff419896973c65110188285a19940683f10301
Jairo <jairo@iv66.net>
2021-02-06 15:12:58 +0800

    [修正]
    1. ＡＧ旗艦廳按左上角返回按鈕統一回到首頁

--- Hybird_1e3c3b/HomePage/Controller/BTTAGQJViewController.m
+++ Hybird_1e3c3b/HomePage/Controller/BTTAGQJViewController.m
@@ -7,69 +7,45 @@
 //
 
 #import "BTTAGQJViewController.h"
-#import "BTTBJLPopView.h"
 
 @interface BTTAGQJViewController ()
-//@property (nonatomic, strong) BTTBJLPopView *customView;
-//@property (nonatomic, assign) BOOL isRemoved;
 @end
 
 @implementation BTTAGQJViewController
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-//    if (!self.isRemoved && self.isViewLoaded) {
-//        self.isRemoved = true;
-//        [[IVGameManager sharedManager].agqjVC removeObserver:self forKeyPath:@"loadStatus" context:nil];
-//    }
 }
+
 - (BOOL)shouldAutorotate
 {
     return [[IVGameManager sharedManager].agqjVC shouldAutorotate];
 }
+
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     return [[IVGameManager sharedManager].agqjVC supportedInterfaceOrientations];
 }
+
 - (UIStatusBarStyle)preferredStatusBarStyle{
     return StatusBarStyle;
 }
+
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:YES animated:animated];
 }
+
 -(void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:animated];
     [self.navigationController setNavigationBarHidden:NO animated:animated];
 }
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     [self addGameViewToSelf];
     [self registerNotifiction];
-//    [[IVGameManager sharedManager].agqjVC addObserver:self forKeyPath:@"loadStatus" options:NSKeyValueObservingOptionOld|NSKeyValueObservingOptionNew context:nil];
-//    self.isRemoved = false;
-}
-//- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-//    if([keyPath isEqualToString:@"loadStatus"]) {
-//        if ([IVGameManager sharedManager].agqjVC.loadStatus == IVGameLoadStatusSuccess) {
-//            if (self.customView == nil) {
-//                [self showBjlPopView];
-//            }
-//        }
-//    }
-//}
-- (void)registerNotifiction {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(finishLoginGame) name:@"FinishLoginGame" object:nil];
 }
 
-- (void)finishLoginGame {
-    if ([IVGameManager sharedManager].agqjVC.loadStatus == IVGameLoadStatusSuccess) {
-        [[CNTimeLog shareInstance] endRecordTime:CNEventAGQJLaunch];
-//        if (self.customView == nil) {
-//            [self showBjlPopView];
-//        }
-    }
-}
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
@@ -77,6 +53,7 @@
         [self addGameViewToSelf];
     }
 }
+
 - (void)viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
@@ -84,9 +61,17 @@
     [[IVGameManager sharedManager].agqjVC.view removeFromSuperview];
     [[IVGameManager sharedManager].agqjVC removeFromParentViewController];
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+
+- (void)registerNotifiction {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(finishLoginGame) name:@"FinishLoginGame" object:nil];
 }
+
+- (void)finishLoginGame {
+    if ([IVGameManager sharedManager].agqjVC.loadStatus == IVGameLoadStatusSuccess) {
+        [[CNTimeLog shareInstance] endRecordTime:CNEventAGQJLaunch];
+    }
+}
+
 - (void)addGameViewToSelf
 {
     IVGameManager *manager = [IVGameManager sharedManager];
@@ -100,6 +85,13 @@
     [IVGameManager sharedManager].agqjVC.view.hidden = NO;
     [[IVGameManager sharedManager].agqjVC didMoveToParentViewController:self];
     [[IVGameManager sharedManager].agqjVC reloadGame];
+    [[IVGameManager sharedManager].agqjVC.backBtn addTarget:self action:@selector(overrideBackAction)];
+}
+
+-(void)overrideBackAction
+{
+    [[UIDevice currentDevice] setValue:@(UIDeviceOrientationPortrait) forKey:@"orientation"];
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 + (void)addGameViewToWindow
@@ -112,19 +104,8 @@
     [IVGameManager sharedManager].agqjVC.view.frame = keyWin.frame;
 }
 
-//-(void)showBjlPopView {
-//    self.customView = [BTTBJLPopView viewFromXib];
-//    self.customView.frame = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-//    BTTAnimationPopView *popView = [[BTTAnimationPopView alloc] initWithCustomView:self.customView popStyle:BTTAnimationPopStyleScale dismissStyle:BTTAnimationDismissStyleNO];
-//    [popView pop];
-//    weakSelf(weakSelf);
-//    self.customView.dismissBlock = ^{
-//        strongSelf(strongSelf);
-//        [popView dismiss];
-//        if (!strongSelf.isRemoved && [[IVGameManager sharedManager].agqjVC observationInfo] != nil) {
-//            strongSelf.isRemoved = true;
-//            [[IVGameManager sharedManager].agqjVC removeObserver:strongSelf forKeyPath:@"loadStatus" context:nil];
-//        }
-//    };
-//}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
 @end
