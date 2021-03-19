cdc1f09cbcac04f077c6f5b34d21205a842a06a0
Jairo <jairo@iv66.net>
2021-03-19 15:18:39 +0800

    [修正]
    1. 更換域名

--- Hybird_1e3c3b/Public/Tools/Lib/Hybird/HAInitConfig.m
+++ Hybird_1e3c3b/Public/Tools/Lib/Hybird/HAInitConfig.m
@@ -17,14 +17,11 @@
 + (NSArray *)gateways {
     switch (EnvirmentType) {
         case 0:
-//            return @[@"http://www.pt-gateway.com/_glaxy_a01_/"];
             return @[@"http://www.pt-gateway.com/_glaxy_1e3c3b_/"];
         case 1:
-//            return @[@"http://fm.918rr.com/_glaxy_a01_/"];
             return @[@"http://fm.918rr.com/_glaxy_1e3c3b_/"];
         default:
-//            return @[@"https://m.918nee.com/_glaxy_a01_/",@"https://m.918nff.com/_glaxy_a01_/"];
-            return @[@"https://m.918nee.com/_glaxy_1e3c3b_/",@"https://m.918nff.com/_glaxy_1e3c3b_/"];
+            return @[@"https://m.918zza.com/_glaxy_1e3c3b_/",@"https://m.918zzb.com/_glaxy_1e3c3b_/"];
     }
 }
 //https://a01mobileimage.sm830.com/static/A01M/_default/__static/_wms/_l/_banner/banner-index-186bd16447a23dc145b57aef0989822cf.jpg?v=d5de123c20360742454ddff7a6a0d94b
@@ -37,7 +34,7 @@
             return @"http://fm.918rr.com/";
             break;
         case 2:
-            return @"https://m.918nee.com/";
+            return @"https://m.918zza.com/";
             break;
         default:
             break;
@@ -53,7 +50,7 @@
             return @"http://fm.918rr.com/";
             break;
         case 2:
-            return @"https://m.918nee.com/";
+            return @"https://m.918zza.com/";
             break;
         default:
             break;
@@ -62,12 +59,9 @@
 
 + (NSString *)appKey {
     return @"";
-//    return @"A01FM";
-//    return @"1682d3a2ee0c4ee8acbe58a5c39bb888";
 }
 
 + (NSString *)appId {
-//    return @"A01APP02";
     return @"d9002994416444bbb6d172e65ee4ce6c";
 //    return @"aac31aaaa28d4ea89cf2a97ff72bf82d";//vip
 }
@@ -96,7 +90,6 @@
 
 + (NSString *)productId {
     return @"A01";
-//    return @"1682d3a2ee0c4ee8acbe58a5c39bb888";
 }
 
 + (NSString *)product3SId {
