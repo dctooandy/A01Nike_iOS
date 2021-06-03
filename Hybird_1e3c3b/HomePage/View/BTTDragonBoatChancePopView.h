bf3407579c78e36f0551cfe5f174aa2937c2dbf7
RM03 <>
2021-06-03 16:53:50 +0800

    [調整]
    新增檔案

---
+++ Hybird_1e3c3b/HomePage/View/BTTDragonBoatChancePopView.h
@@ -0,0 +1,19 @@
+//
+//  BTTDragonBoatChancePopView.h
+//  Hybird_1e3c3b
+//
+//  Created by Andy on 6/3/21.
+//  Copyright © 2021 BTT. All rights reserved.
+//
+
+#import "BTTBaseAnimationPopView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BTTDragonBoatChancePopView : BTTBaseAnimationPopView
+
+- (void)configForAmount:(NSInteger)amountValue;
+
+@end
+
+NS_ASSUME_NONNULL_END
