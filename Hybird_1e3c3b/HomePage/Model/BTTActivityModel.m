613f7617b68ebd3a966bafa91b1723fabc9416d1
RM03 <>
2021-09-02 16:39:32 +0800

    [調整]
    品牌活動 大小調整

--- Hybird_1e3c3b/HomePage/Model/BTTActivityModel.m
+++ Hybird_1e3c3b/HomePage/Model/BTTActivityModel.m
@@ -17,12 +17,37 @@
 - (CGFloat)cellHeight {
     _cellHeight = 0;
     CGFloat imageHeight = 0;
-    if (SCREEN_WIDTH == 320) {
-        imageHeight = 180;
-    } else if (SCREEN_WIDTH == 375) {
-        imageHeight = 200;
-    } else if (SCREEN_WIDTH == 414 || KIsiPhoneX) {
-        imageHeight = 220;
+//    if (SCREEN_WIDTH == 320) {
+//        imageHeight = 180;
+//    } else if (SCREEN_WIDTH == 375) {
+//        imageHeight = 200;
+//    } else if (SCREEN_WIDTH == 414 || KIsiPhoneX) {
+//        imageHeight = 220;
+//    } else
+//    {
+//        imageHeight = (200 * SCREEN_WIDTH/375);
+//    }
+    if (self.imageUrls.count)
+    {
+        //無限排列
+//        if ((self.imageUrls.count %3) == 0)
+//        {
+//            imageHeight = 20 + (self.imageUrls.count / 3) * ((SCREEN_WIDTH - 40)/3 + 10) - 10;
+//        }else{
+//            imageHeight = 20 + ((self.imageUrls.count / 3) + 1) * ((SCREEN_WIDTH - 40)/3 + 10) - 10;
+//        }
+        if (self.imageUrls.count<9)
+        {
+            if ((self.imageUrls.count %3) == 0)
+            {
+                imageHeight = 20 + (self.imageUrls.count / 3) * ((SCREEN_WIDTH - 40)/3 + 10) - 10;
+            }else{
+                imageHeight = 20 + ((self.imageUrls.count / 3) + 1) * ((SCREEN_WIDTH - 40)/3 + 10) - 10;
+            }
+        }else
+        {
+            imageHeight = 20 + 3 * ((SCREEN_WIDTH - 40)/3 + 10) - 10;
+        }
     }
     _cellHeight += (imageHeight + 102 + self.descHeight);
     return _cellHeight;
