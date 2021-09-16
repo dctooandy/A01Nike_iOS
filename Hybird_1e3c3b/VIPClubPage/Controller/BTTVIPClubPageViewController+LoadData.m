285c359d0f4e1464ebac2aedddf0296537011ae9
RM03 <>
2021-09-16 15:03:54 +0800

    [修正]
    加上/index.html

--- Hybird_1e3c3b/VIPClubPage/Controller/BTTVIPClubPageViewController+LoadData.m
+++ Hybird_1e3c3b/VIPClubPage/Controller/BTTVIPClubPageViewController+LoadData.m
@@ -124,24 +124,38 @@ static const char *BTTNextGroupKey = "nextGroup";
     VIPHistorySideBarModel *sData7 = [[VIPHistorySideBarModel alloc] initWithYearString:@"2013" withImageName:@"logo" withIsSelected:NO withIsFirstData:NO];
 
 
-    VIPHistoryImageModel *iData1 = [[VIPHistoryImageModel alloc] initWithYearString:@"2019" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img44.258ddc53.jpg" withUrl:@"www/promo_av_201910" withTopTitleString:@"我和三上悠亚有个约会" withSubTitleString:@"与三上悠亚的浪漫邂逅圆满结束" withIsFirstData:YES withIsDetails:YES];
-    VIPHistoryImageModel *iData11 = [[VIPHistoryImageModel alloc] initWithYearString:@"2019" WithMonthString:@"02" withImageName:@"cdn/1e3c3bFM/static/img/img43.e4a7c16a.jpg" withUrl:@"www/yacht_tour_201902" withTopTitleString:@"四天三夜浪漫游艇邂逅之旅" withSubTitleString:@"浪漫游艇邂逅之旅圆满结束" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData1 = [[VIPHistoryImageModel alloc] initWithYearString:@"2019" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img44.258ddc53.jpg" withUrl:@"www/promo_av_201910/index.html"
+                                                                 withTopTitleString:@"我和三上悠亚有个约会" withSubTitleString:@"与三上悠亚的浪漫邂逅圆满结束" withIsFirstData:YES withIsDetails:YES];
+    VIPHistoryImageModel *iData11 = [[VIPHistoryImageModel alloc] initWithYearString:@"2019" WithMonthString:@"02" withImageName:@"cdn/1e3c3bFM/static/img/img43.e4a7c16a.jpg" withUrl:@"www/yacht_tour_201902/index.html"
+                                                                  withTopTitleString:@"四天三夜浪漫游艇邂逅之旅" withSubTitleString:@"浪漫游艇邂逅之旅圆满结束" withIsFirstData:NO withIsDetails:YES];
     
-    VIPHistoryImageModel *iData2 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img42.ea77e007.jpg" withUrl:@"www/f1_race_2018_3" withTopTitleString:@"五天四夜F1日本之旅" withSubTitleString:@"观战F1&日本关西韵味之旅活动圆满收官" withIsFirstData:YES withIsDetails:YES];
-    VIPHistoryImageModel *iData21 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img41.205503c9.jpg" withUrl:@"www/yacht_bunny2" withTopTitleString:@"三天两夜兔女郎豪华游艇游" withSubTitleString:@"豪华游艇邂逅兔女郎主题旅游活动圆满结束" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData22 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"06" withImageName:@"cdn/1e3c3bFM/static/img/img40.3f962838.png" withUrl:@"www/dream_island_trip2" withTopTitleString:@"“菲”去不可  科隆岛浪漫之旅" withSubTitleString:@"阳光、沙滩、海岛、比基尼美女，好玩到不想走！" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData23 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"04" withImageName:@"cdn/1e3c3bFM/static/img/img39.de303dfc.jpg" withUrl:@"www/bayer_travel_4thquarter2" withTopTitleString:@"激情观战勒沃库森旅游" withSubTitleString:@"2018年4月勒沃库森旅游季，德法浪漫游再次让博天堂客户欢聚" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData2 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img42.ea77e007.jpg" withUrl:@"www/f1_race_2018_3/index.html"
+                                                                 withTopTitleString:@"五天四夜F1日本之旅" withSubTitleString:@"观战F1&日本关西韵味之旅活动圆满收官" withIsFirstData:YES withIsDetails:YES];
+    VIPHistoryImageModel *iData21 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img41.205503c9.jpg" withUrl:@"www/yacht_bunny2/index.html"
+                                                                  withTopTitleString:@"三天两夜兔女郎豪华游艇游" withSubTitleString:@"豪华游艇邂逅兔女郎主题旅游活动圆满结束" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData22 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"06" withImageName:@"cdn/1e3c3bFM/static/img/img40.3f962838.png" withUrl:@"www/dream_island_trip2/index.html"
+                                                                  withTopTitleString:@"“菲”去不可  科隆岛浪漫之旅" withSubTitleString:@"阳光、沙滩、海岛、比基尼美女，好玩到不想走！" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData23 = [[VIPHistoryImageModel alloc] initWithYearString:@"2018" WithMonthString:@"04" withImageName:@"cdn/1e3c3bFM/static/img/img39.de303dfc.jpg" withUrl:@"www/bayer_travel_4thquarter2/index.html" withTopTitleString:@"激情观战勒沃库森旅游" withSubTitleString:@"2018年4月勒沃库森旅游季，德法浪漫游再次让博天堂客户欢聚" withIsFirstData:NO withIsDetails:YES];
     
-    VIPHistoryImageModel *iData3 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"12" withImageName:@"cdn/1e3c3bFM/static/img/img_38.72c607b4.jpg" withUrl:@"www/ph_tour" withTopTitleString:@"“飞”律宾第四季" withSubTitleString:@"长滩岛遇见博天堂 完美收官" withIsFirstData:YES withIsDetails:YES];
-    VIPHistoryImageModel *iData31 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"8" withImageName:@"cdn/1e3c3bFM/static/img/img37.d8733923.jpg" withUrl:@"www/dream_island_trip" withTopTitleString:@"“飞”律宾第三季" withSubTitleString:@"梦幻海岛之旅" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData32 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"06" withImageName:@"cdn/1e3c3bFM/static/img/img_as.0016dca6.jpg" withUrl:@"" withTopTitleString:@"新增AS电玩城" withSubTitleString:@"AS厅首创实时实体赌场数据开牌，社交化在线博彩。" withIsFirstData:NO withIsDetails:NO];
-    VIPHistoryImageModel *iData33 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"05" withImageName:@"cdn/1e3c3bFM/static/img/img38.5927338e.jpg" withUrl:@"www/yacht_tour2" withTopTitleString:@"“飞”律宾第二季" withSubTitleString:@"欢乐翱翔之旅" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData34 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"02" withImageName:@"cdn/1e3c3bFM/static/img/img39.de303dfc.jpg" withUrl:@"www/yacht_tour" withTopTitleString:@"“飞”律宾第一季" withSubTitleString:@"惊喜游艇之旅" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData3 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"12" withImageName:@"cdn/1e3c3bFM/static/img/img_38.72c607b4.jpg" withUrl:@"www/ph_tour/index.html"
+                                                                 withTopTitleString:@"“飞”律宾第四季" withSubTitleString:@"长滩岛遇见博天堂 完美收官" withIsFirstData:YES withIsDetails:YES];
+    VIPHistoryImageModel *iData31 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"8" withImageName:@"cdn/1e3c3bFM/static/img/img37.d8733923.jpg" withUrl:@"www/dream_island_trip/index.html"
+                                                                  withTopTitleString:@"“飞”律宾第三季" withSubTitleString:@"梦幻海岛之旅" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData32 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"06" withImageName:@"cdn/1e3c3bFM/static/img/img_as.0016dca6.jpg" withUrl:@""
+                                                                  withTopTitleString:@"新增AS电玩城" withSubTitleString:@"AS厅首创实时实体赌场数据开牌，社交化在线博彩。" withIsFirstData:NO withIsDetails:NO];
+    VIPHistoryImageModel *iData33 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"05" withImageName:@"cdn/1e3c3bFM/static/img/img38.5927338e.jpg" withUrl:@"www/yacht_tour2/index.html"
+                                                                  withTopTitleString:@"“飞”律宾第二季" withSubTitleString:@"欢乐翱翔之旅" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData34 = [[VIPHistoryImageModel alloc] initWithYearString:@"2017" WithMonthString:@"02" withImageName:@"cdn/1e3c3bFM/static/img/img39.de303dfc.jpg" withUrl:@"www/yacht_tour/index.html"
+                                                                  withTopTitleString:@"“飞”律宾第一季" withSubTitleString:@"惊喜游艇之旅" withIsFirstData:NO withIsDetails:YES];
     
-    VIPHistoryImageModel *iData4 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img1.e15119f4.jpg" withUrl:@"" withTopTitleString:@"AG旗舰厅新增贵宾百万投注桌台" withSubTitleString:@"～百万单注想投就投 盈利机会不容错过～" withIsFirstData:YES withIsDetails:NO];
-    VIPHistoryImageModel *iData41 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img2.d4bf9528.jpg" withUrl:@"www/bayer_tab4" withTopTitleString:@"德荷两国欢乐游 现场观战勒 | 沃库森" withSubTitleString:@"观看精彩刺激球赛 尊享贵宾奢华待遇" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData42 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img34.ba4025ae.png" withUrl:@"" withTopTitleString:@"AG旗舰厅新增30万盘口" withSubTitleString:@"AG旗舰厅真人单注30万盘口火热开启" withIsFirstData:NO withIsDetails:NO];
-    VIPHistoryImageModel *iData43 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img3.9866a554.jpg" withUrl:@"" withTopTitleString:@"真人娱乐开设50、100万盘口，单日提款限额提升至1000万" withSubTitleString:@"50万盘口开设无任何要求,1000万提款工作日1小时到账" withIsFirstData:NO withIsDetails:NO];
+    VIPHistoryImageModel *iData4 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img1.e15119f4.jpg" withUrl:@""
+                                                                 withTopTitleString:@"AG旗舰厅新增贵宾百万投注桌台" withSubTitleString:@"～百万单注想投就投 盈利机会不容错过～" withIsFirstData:YES withIsDetails:NO];
+    VIPHistoryImageModel *iData41 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img2.d4bf9528.jpg" withUrl:@"www/bayer_tab4/index.html"
+                                                                  withTopTitleString:@"德荷两国欢乐游 现场观战勒 | 沃库森" withSubTitleString:@"观看精彩刺激球赛 尊享贵宾奢华待遇" withIsFirstData:NO withIsDetails:YES];
+    VIPHistoryImageModel *iData42 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img34.ba4025ae.png" withUrl:@""
+                                                                  withTopTitleString:@"AG旗舰厅新增30万盘口" withSubTitleString:@"AG旗舰厅真人单注30万盘口火热开启" withIsFirstData:NO withIsDetails:NO];
+    VIPHistoryImageModel *iData43 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img3.9866a554.jpg" withUrl:@""
+                                                                  withTopTitleString:@"真人娱乐开设50、100万盘口，单日提款限额提升至1000万" withSubTitleString:@"50万盘口开设无任何要求,1000万提款工作日1小时到账" withIsFirstData:NO withIsDetails:NO];
     VIPHistoryImageModel *iData44 = [[VIPHistoryImageModel alloc] initWithYearString:@"2016" WithMonthString:@"09" withImageName:@"cdn/1e3c3bFM/static/img/img4.50d7a166.jpg" withUrl:@""
                                                                   withTopTitleString:@"AG旗舰豪华厅正式上线"
                                                                   withSubTitleString:@"亚洲新坐标 全球首推" withIsFirstData:NO withIsDetails:NO];
@@ -185,26 +199,26 @@ static const char *BTTNextGroupKey = "nextGroup";
     VIPHistoryImageModel *iData53 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"07" withImageName:@"cdn/1e3c3bFM/static/img/img17.e3d66b49.jpg" withUrl:@""
                                                                   withTopTitleString:@"博天堂全新呈现HG真人游戏"
                                                                   withSubTitleString:@"HG平台闪亮登场 美女荷官现场互动" withIsFirstData:NO withIsDetails:NO];
-    VIPHistoryImageModel *iData54 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"04" withImageName:@"cdn/1e3c3bFM/static/img/img18.046692ad.jpg" withUrl:@"www/h_helpspanish"
+    VIPHistoryImageModel *iData54 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"04" withImageName:@"cdn/1e3c3bFM/static/img/img18.046692ad.jpg" withUrl:@"www/h_helpspanish/index.html"
                                                                   withTopTitleString:@"博天堂助力西甲"
                                                                   withSubTitleString:@"博天堂力捧巴萨卫冕西甲冠军，再创辉煌" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData55 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"03" withImageName:@"cdn/1e3c3bFM/static/img/img19.bfc6a5f2.jpg" withUrl:@"www/h_polygramconcert"
+    VIPHistoryImageModel *iData55 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"03" withImageName:@"cdn/1e3c3bFM/static/img/img19.bfc6a5f2.jpg" withUrl:@"www/h_polygramconcert/index.html"
                                                                   withTopTitleString:@"博天堂集团独家冠名赞助"
                                                                   withSubTitleString:@"宝丽金永恒金曲澳门演唱会，博天堂上百名客户聚首澳门" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData56 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"03" withImageName:@"cdn/1e3c3bFM/static/img/img20.a35ebcd8.jpg" withUrl:@"www/h_thankyou"
+    VIPHistoryImageModel *iData56 = [[VIPHistoryImageModel alloc] initWithYearString:@"2015" WithMonthString:@"03" withImageName:@"cdn/1e3c3bFM/static/img/img20.a35ebcd8.jpg" withUrl:@"www/h_thankyou/index.html"
                                                                   withTopTitleString:@"“一路相伴 感谢有您”"
                                                                   withSubTitleString:@"博天堂澳门春茗答谢晚宴 派送百万豪礼" withIsFirstData:NO withIsDetails:YES];
     
-    VIPHistoryImageModel *iData6 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"11" withImageName:@"cdn/1e3c3bFM/static/img/img21.f7f119c3.jpg" withUrl:@"www/h_golfpackage"
+    VIPHistoryImageModel *iData6 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"11" withImageName:@"cdn/1e3c3bFM/static/img/img21.f7f119c3.jpg" withUrl:@"www/h_golfpackage/index.html"
                                                                   withTopTitleString:@"918.com高尔夫亚巡赛完美收官"
                                                                   withSubTitleString:@"新加坡老将马玛（Mardan Mamat）喜获冠军" withIsFirstData:YES withIsDetails:YES];
     VIPHistoryImageModel *iData61 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img22.732edff1.jpg" withUrl:@""
                                                                   withTopTitleString:@"互动咪牌百家乐正式上线"
                                                                   withSubTitleString:@"互动咪牌百家乐正式上线，LED超清视觉效果 游戏进程尽在掌控" withIsFirstData:NO withIsDetails:NO];
-    VIPHistoryImageModel *iData62 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img23.9e74feb6.jpg" withUrl:@"www/h_golftour"
+    VIPHistoryImageModel *iData62 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"10" withImageName:@"cdn/1e3c3bFM/static/img/img23.9e74feb6.jpg" withUrl:@"www/h_golftour/index.html"
                                                                   withTopTitleString:@"博天堂冠名2014高尔夫亚巡赛"
                                                                   withSubTitleString:@"10月10日举办南木球场高球友谊赛及马尼拉站媒体见面会" withIsFirstData:NO withIsDetails:YES];
-    VIPHistoryImageModel *iData63 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"7" withImageName:@"cdn/1e3c3bFM/static/img/img24.56aac11b.jpg" withUrl:@"www/h_golfconference"
+    VIPHistoryImageModel *iData63 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"7" withImageName:@"cdn/1e3c3bFM/static/img/img24.56aac11b.jpg" withUrl:@"www/h_golfconference/index.html"
                                                                   withTopTitleString:@"“918.com亚巡大师杯”高尔夫赛新闻发布会"
                                                                   withSubTitleString:@"在菲律宾最知名赌场“resorts world”召开新闻发布会" withIsFirstData:NO withIsDetails:YES];
     VIPHistoryImageModel *iData64 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"7" withImageName:@"cdn/1e3c3bFM/static/img/img25.2ab4cb56.jpg" withUrl:@""
@@ -213,14 +227,14 @@ static const char *BTTNextGroupKey = "nextGroup";
     VIPHistoryImageModel *iData65 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"5" withImageName:@"cdn/1e3c3bFM/static/img/img26.c5c36c82.jpg" withUrl:@""
                                                                   withTopTitleString:@"博天堂正式更换LOGO"
                                                                   withSubTitleString:@"918博天堂 博彩天堂" withIsFirstData:NO withIsDetails:NO];
-    VIPHistoryImageModel *iData66 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"1" withImageName:@"cdn/1e3c3bFM/static/img/img27.b1037f51.jpg" withUrl:@"www/h_lotusofficer"
+    VIPHistoryImageModel *iData66 = [[VIPHistoryImageModel alloc] initWithYearString:@"2014" WithMonthString:@"1" withImageName:@"cdn/1e3c3bFM/static/img/img27.b1037f51.jpg" withUrl:@"www/h_lotusofficer/index.html"
                                                                   withTopTitleString:@"博天堂AV女优荷官周"
                                                                   withSubTitleString:@"一睹波多野结衣等著名AV女优荷官发牌风采" withIsFirstData:NO withIsDetails:YES];
     
     VIPHistoryImageModel *iData7 = [[VIPHistoryImageModel alloc] initWithYearString:@"2013" WithMonthString:@"12" withImageName:@"cdn/1e3c3bFM/static/img/img28.e42cc206.jpg" withUrl:@""
                                                                   withTopTitleString:@"博天堂引领博彩业潮流"
                                                                   withSubTitleString:@"正式开通比特币充值游戏" withIsFirstData:YES withIsDetails:NO];
-    VIPHistoryImageModel *iData71 = [[VIPHistoryImageModel alloc] initWithYearString:@"2013" WithMonthString:@"11" withImageName:@"cdn/1e3c3bFM/static/img/img29.e1db268c.jpg" withUrl:@"www/h_industrytrend"
+    VIPHistoryImageModel *iData71 = [[VIPHistoryImageModel alloc] initWithYearString:@"2013" WithMonthString:@"11" withImageName:@"cdn/1e3c3bFM/static/img/img29.e1db268c.jpg" withUrl:@"www/h_industrytrend/index.html"
                                                                   withTopTitleString:@"博天堂爱心捐赠"
                                                                   withSubTitleString:@"博天堂全体爱心捐赠100万peso" withIsFirstData:NO withIsDetails:YES];
     VIPHistoryImageModel *iData72 = [[VIPHistoryImageModel alloc] initWithYearString:@"2013" WithMonthString:@"11" withImageName:@"cdn/1e3c3bFM/static/img/img29.e1db268c.jpg" withUrl:@""
