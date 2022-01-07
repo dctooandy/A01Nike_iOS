27d56aacc26280c387228500f09e4927a3b677fa
andy <andy@ct88.com.tw>
2022-01-07 16:36:43 +0800

    [调整]
    新增 倒数页面

---
+++ Hybird_1e3c3b/HomePage/View/RedPacketsPreView.h
@@ -0,0 +1,17 @@
+//
+//  RedPacketsPreView.h
+//  Hybird_1e3c3b
+//
+//  Created by RM03 on 2022/1/7.
+//  Copyright © 2022 BTT. All rights reserved.
+//
+
+#import "BTTBaseAnimationPopView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RedPacketsPreView : BTTBaseAnimationPopView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hybird_1e3c3b/HomePage/View/RedPacketsPreView.m
@@ -0,0 +1,23 @@
+//
+//  RedPacketsPreView.m
+//  Hybird_1e3c3b
+//
+//  Created by RM03 on 2022/1/7.
+//  Copyright © 2022 BTT. All rights reserved.
+//
+
+#import "RedPacketsPreView.h"
+
+@implementation RedPacketsPreView
+- (IBAction)joinRedPacketAction:(id)sender {
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
