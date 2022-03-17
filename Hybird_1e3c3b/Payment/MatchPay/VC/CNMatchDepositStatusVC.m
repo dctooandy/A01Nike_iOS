05baf799477de4236f6150b2f125924e8bb3f64f
cean <>
2022-03-17 11:32:33 +0800

    存款成功不提示

--- Hybird_1e3c3b/Payment/MatchPay/VC/CNMatchDepositStatusVC.m
+++ Hybird_1e3c3b/Payment/MatchPay/VC/CNMatchDepositStatusVC.m
@@ -186,10 +186,7 @@ typedef NS_ENUM(NSUInteger, CNMPayUIStatus) {
         if ([response isKindOfClass:[NSDictionary class]]) {
             NSDictionary *dic = (NSDictionary *)response;
             if ([[dic objectForKey:@"code"] isEqualToString:@"00000"]) {
-                [weakSelf showSuccess:@"提交成功"];
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                    [weakSelf.navigationController popToRootViewControllerAnimated:YES];
-                });
+                [weakSelf.navigationController popToRootViewControllerAnimated:YES];
             } else {
                 [self showError:[dic objectForKey:@"message"]];
             }
