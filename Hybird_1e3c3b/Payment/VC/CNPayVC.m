5b5cb4ad64464ef65a7a9e298010347c844b330c
cean <>
2022-03-25 15:13:02 +0800

    新增惊喜UI 修改优惠UI

--- Hybird_1e3c3b/Payment/VC/CNPayVC.m
+++ Hybird_1e3c3b/Payment/VC/CNPayVC.m
@@ -109,10 +109,10 @@
 - (void)showTradeBill {
     CNMBillView *view = [[CNMBillView alloc] init];
     if (self.fastModel.mmProcessingOrderStatus == 2) {
-        [view.statusBtn setTitle:@"确认存款" forState:UIControlStateNormal];
+        [view setPromoTag:NO];
         [view.statusBtn addTarget:self action:@selector(confirmBill) forControlEvents:UIControlEventTouchUpInside];
     } else {
-        [view.statusBtn setTitle:@"我要催单" forState:UIControlStateNormal];
+        [view setPromoTag:YES];
         [view.statusBtn addTarget:self action:@selector(showUploadUI) forControlEvents:UIControlEventTouchUpInside];
     }
     self.billViewH.constant = 66;
